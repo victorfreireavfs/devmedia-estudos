f0bc7d177c9430e6eb1e82185c4bc14c76fbc6d0
Victor Freire <128263774+victorfreireavfs@users.noreply.github.com>
2024-05-24 20:12:00 -0300

    Coleção de Objetos
    - usando Math.random() e Math.floor() para sortear números aleatórios e exibir as propriedades da variável

--- exercicios-js/ex006ColecaoDeObjetos.js
+++ exercicios-js/ex006ColecaoDeObjetos.js
@@ -16,27 +16,56 @@
 
 //--------------------------------------------
 
-var colecao_series_programacao = [
-    { nome: "Breaking Bad",           horario: "21h",     sinopse: "Um professor de química se transforma quando descobre ter um câncer terminal. Daí ele usa suas habilidades a favor do crime"},
-    { nome: "Fargo",                  horario: "22h",     sinopse: "Uma sequência de crimes saem errado e são investigados por uma detetive."},
-    { nome: "Lost",                   horario: "20h",     sinopse: "Um avião cai em uma ilha deserta e logo um grupo de passageiros precisa lutar para sobreviver." },
-    { nome: "Prison Break",           horario: "23h",     sinopse: "Um homem cria um plano para tirar o irmão sentenciado à morte por um suposto assassinato do vice-presidente dos EUA"},
-    { nome: "Black Mirror",           horario: "23h",     sinopse: "Contos de ficção científica que refletem o lado negro da tecnologia, mostrando que nem toda novidade traz só benefícios." },
-    { nome: "Pessoa de interesse ",   horario: "20h",     sinopse: "Um ex-agente da CIA, dado como morto pelo governo dos EUA, é recrutado por um milionário, para um projeto ultrassecreto."},
-    { nome: "Dark",                   horario: "22h",     sinopse: "O desaparecimento de crianças na cidade alemã de Winden remete a acontecimentos idênticos ocorridos há 33 anos e 66 anos."}
+// var colecao_series_programacao = [
+//     { nome: "Breaking Bad",           horario: "21h",     sinopse: "Um professor de química se transforma quando descobre ter um câncer terminal. Daí ele usa suas habilidades a favor do crime"},
+//     { nome: "Fargo",                  horario: "22h",     sinopse: "Uma sequência de crimes saem errado e são investigados por uma detetive."},
+//     { nome: "Lost",                   horario: "20h",     sinopse: "Um avião cai em uma ilha deserta e logo um grupo de passageiros precisa lutar para sobreviver." },
+//     { nome: "Prison Break",           horario: "23h",     sinopse: "Um homem cria um plano para tirar o irmão sentenciado à morte por um suposto assassinato do vice-presidente dos EUA"},
+//     { nome: "Black Mirror",           horario: "23h",     sinopse: "Contos de ficção científica que refletem o lado negro da tecnologia, mostrando que nem toda novidade traz só benefícios." },
+//     { nome: "Pessoa de interesse ",   horario: "20h",     sinopse: "Um ex-agente da CIA, dado como morto pelo governo dos EUA, é recrutado por um milionário, para um projeto ultrassecreto."},
+//     { nome: "Dark",                   horario: "22h",     sinopse: "O desaparecimento de crianças na cidade alemã de Winden remete a acontecimentos idênticos ocorridos há 33 anos e 66 anos."}
+// ];
+
+// var data_atual = new Date(); // guardando o valor da data atual numa variável
+
+// var dia_semana = data_atual.getDay(); 
+// // get.day() retorna o dia da semana com valores de 0 a 6. usamos o valor retornado pra acessar o objeto da coleção
+
+// var serie_do_dia = colecao_series_programacao[dia_semana];
+
+// var nome_serie = serie_do_dia.nome;
+// var horario_serie = serie_do_dia.horario;
+// var sinopse_serie = serie_do_dia.sinopse;
+// // guardando os dados do objeto seria_do_dia em variáveis
+
+// console.log("Hoje é dia de " + nome_serie + " às " + horario_serie);
+// console.log("A seguir uma visão geral da série: " + sinopse_serie);
+
+
+// --------------------------------------------------
+
+
+var curiosidades_chuck_norris = [
+    { titulo: "Cuidados com a higiene bucal",           conteudo: "Chuck Norris usa arame farpado como fio dental."},
+    { titulo: "Fórmula para maratonar séries",          conteudo: "Chuck Norris pode assistir um episódio de 60 minutos em 22 segundos."},
+    { titulo: "Suicida que não morre",                  conteudo: "Chuck Norris foi homem-bomba 34 vezes."},
+    { titulo: "Olhos que tudo veem",                    conteudo: "Chuck Norris já viu o homem invisível."},
+    { titulo: "Manipulando o tempo",                    conteudo: "Chuck Norris não usa relógio. Ele decide que horas são."},
+    { titulo: "Praticando esportes radicais",           conteudo: "Chuck Norris faz bungee jump sem corda."},
+    { titulo: "Não vale chorar",                        conteudo: "Chuck Norris faz cebolas chorarem."},
+    { titulo: "Tempero de fogo",                        conteudo: "Chuck Norris usa pólvora como tempero."},
+    { titulo: "Extinção dos dinossauros",               conteudo: "Chuck Norris encarou os dinossauros uma vez, apenas uma."},
+    { titulo: "Contando sem parar",                     conteudo: "Chuck Norris contou até o infinito. Duas vezes."}
 ];
 
-var data_atual = new Date(); // guardando o valor da data atual numa variável
+var tamanho_colecao = curiosidades_chuck_norris.length;
+var numero_sorteado = Math.floor(Math.random() * tamanho_colecao);
 
-var dia_semana = data_atual.getDay(); 
-// get.day() retorna o dia da semana com valores de 0 a 6. usamos o valor retornado pra acessar o objeto da coleção
+var curiosidade_escolhida = curiosidades_chuck_norris[numero_sorteado];
 
-var serie_do_dia = colecao_series_programacao[dia_semana];
+var titulo_curiosidade = curiosidade_escolhida.titulo;
+var conteudo_curiosidade = curiosidade_escolhida.conteudo;
 
-var nome_serie = serie_do_dia.nome;
-var horario_serie = serie_do_dia.horario;
-var sinopse_serie = serie_do_dia.sinopse;
-// guardando os dados do objeto seria_do_dia em variáveis
-
-console.log("Hoje é dia de " + nome_serie + " às " + horario_serie);
-console.log("A seguir uma visão geral da série: " + sinopse_serie);
+console.log("CURIOSIDADES SOBRE CHUCK NORRIS");
+console.log("Título: " + titulo_curiosidade );
+console.log("Conteúdo: " + conteudo_curiosidade);
