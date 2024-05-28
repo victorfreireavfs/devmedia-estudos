5fae9462345e4974da01e626c7da497e4f65c1b6
Victor Freire <128263774+victorfreireavfs@users.noreply.github.com>
2024-05-28 12:07:32 -0300

    Input
    - utilizando inputs para cáuculos

--- exercicios-js/inputs/testeReadline.js
+++ exercicios-js/inputs/testeReadline.js
@@ -1,4 +1,16 @@
 import entradaDados from 'readline-sync';
 
-let nome = entradaDados.question('Digite seu nome: ');
-console.log('Olá, ' + nome);
+// let nome = entradaDados.question('Digite seu nome: ');
+// console.log('Olá, ' + nome);
+
+
+// --------------------
+
+console.log('Área do triângulo!')
+
+let base = entradaDados.question('Informe a base: ');
+let altura = entradaDados.question('informe a altura: ');
+
+let area = (base * altura) / 2;
+
+console.log('A área deste triângulo é: ' + area);
