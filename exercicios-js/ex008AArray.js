a27f86639db9053dc545ab2f7faefb46c7272a97
Victor Freire <128263774+victorfreireavfs@users.noreply.github.com>
2024-05-29 20:14:43 -0300

    Arrays
    - acessando índices de um array

---
+++ exercicios-js/ex008AArray.js
@@ -0,0 +1,9 @@
+var notas_semestre = [6,6,6,9,10];
+
+var total_notas = notas_semestre.length;
+var soma_notas = notas_semestre[0] + notas_semestre[1] + notas_semestre[2] + notas_semestre[3] + notas_semestre[4];
+
+var media_notas = soma_notas / total_notas;
+
+console.log('A média de notas é: ' + media_notas);
+
