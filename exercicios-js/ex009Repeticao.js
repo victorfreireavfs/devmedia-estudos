4a5f92a962086916a5e47be1322f67acd73cb978
Victor Freire <128263774+victorfreireavfs@users.noreply.github.com>
2024-05-30 15:03:01 -0300

    Estruturas de Repetição
    - while
    - percorrendo array com while

---
+++ exercicios-js/ex009Repeticao.js
@@ -0,0 +1,38 @@
+// let numero = 3;
+// let contador = 0;
+
+// console.log("Tabuada de multiplicação - Nº " + numero);
+
+// while(contador <= 10){
+    
+//     let resultado = numero * contador;
+
+//     console.log( numero + " x " + contador + " = " + resultado);
+//     contador++
+// }
+
+// ---------------------------
+
+//  Percorrendo array com while
+
+let numeros = [10, 5, 7, 8, 9, 6, 12, 4];
+
+let total_impares = 0;
+let total_pares = 0;
+
+let contador = 0;
+
+while ( contador < numeros.length ) {
+
+    if( numeros[contador] % 2 == 0 ){
+        total_pares++;
+    } else {
+        total_impares++;
+    }
+
+    contador++;
+
+}
+
+console.log(" O total de números ímpares é: " + total_impares);
+console.log(" O total de números pares é: " + total_pares);
