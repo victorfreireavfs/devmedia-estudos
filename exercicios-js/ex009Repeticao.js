7d4f42eb70f31a4ebb043a73d6d1cf92551e51b9
Victor Freire <128263774+victorfreireavfs@users.noreply.github.com>
2024-05-30 21:55:49 -0300

    Estruturas de Repetição
    - do while

--- exercicios-js/ex009Repeticao.js
+++ exercicios-js/ex009Repeticao.js
@@ -15,24 +15,44 @@
 
 //  Percorrendo array com while
 
+// let numeros = [10, 5, 7, 8, 9, 6, 12, 4, 45,39, 845];
+
+// let total_impares = 0;
+// let total_pares = 0;
+
+// let contador = 0;
+
+// while ( contador < numeros.length ) {
+
+//     if( numeros[contador] % 2 == 0 ){
+//         total_pares++;
+//     } else {
+//         total_impares++;
+//     }
+
+//     contador++;
+
+// }
+
+// console.log(" O total de números ímpares é: " + total_impares);
+// console.log(" O total de números pares é: " + total_pares);
+
 let numeros = [10, 5, 7, 8, 9, 6, 12, 4];
 
-let total_impares = 0;
-let total_pares = 0;
+let impares = 0;
+let pares = 0;
 
 let contador = 0;
 
-while ( contador < numeros.length ) {
+while (contador < numeros.length) {
 
-    if( numeros[contador] % 2 == 0 ){
-        total_pares++;
-    } else {
-        total_impares++;
+    if(numeros[contador] % 2 == 0){
+        pares++
+    } else{
+        impares++
     }
-
-    contador++;
-
+    contador++
 }
 
-console.log(" O total de números ímpares é: " + total_impares);
-console.log(" O total de números pares é: " + total_pares);
+console.log(" O total de números ímpares é: " + impares);
+console.log(" O total de números pares é: " + pares);
