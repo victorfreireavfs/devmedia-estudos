b5ef240e8c8a5658ee47b597b1d263b2ff19cf11
Victor Freire <128263774+victorfreireavfs@users.noreply.github.com>
2024-06-18 22:32:22 -0300

    Função Objeto Date
    - praticando como usar os métodos Setters

--- exercicios-js/ex020Date.js
+++ exercicios-js/ex020Date.js
@@ -1,6 +1,12 @@
-const hoje = new Date();
+// const hoje = new Date();
 
-console.log(hoje.toTimeString()); 
+// const options = {
+//     weekday: 'long',
+//     year: 'numeric',
+//     month:'long',
+//     day:'numeric'
+// }
+// console.log(hoje.toLocaleString('pt', options)); 
 
 
 // const hoje = new Date();
@@ -26,3 +32,43 @@ console.log(hoje.toTimeString());
 // console.log( dataAtual.getTime() );
 // console.log( dataAtual.getTimezoneOffset() );
 // console.log( dataAtual.valueOf() );
+
+
+const personalizandoData =  {
+    weekday: 'long', // segunda-feira
+    month: "2-digit", // 02
+    year: '2-digit', // 20
+    day: '2-digit', // 01
+  };
+  console.log(dataAtual.toLocaleDateString('pt-BR', personalizandoData));
+  // segunda-feira, 09/11/20
+
+  const personalizandoTempo = {
+    hour: 'numeric', // 01
+    minute: 'numeric', // 05
+    second: 'numeric', // 09
+  }
+  console.log( dataAtual.toLocaleTimeString('pt-BR', personalizandoTempo) );
+  // 01:05:09
+
+  const personalizandoDataTempo = {
+    dateStyle: "medium", // 9 de nov. de 2020
+    timeStyle: "short", // 01:05
+  }
+  console.log( dataAtual.toLocaleString('pt-BR', personalizandoDataTempo) );
+  // 9 de nov. de 2020 01:05
+
+  const MesAno = {
+      month: "numeric",
+      year: 'numeric',
+  }
+  console.log( dataAtual.toLocaleString('pt-BR', MesAno) );
+  //  11/2020
+
+  const SemanaHora = {
+    weekday: "long",
+    hour: 'numeric',
+    minute: 'numeric',
+  }
+  console.log( dataAtual.toLocaleString('pt-BR', SemanaHora) );
+  // Wednesday 2:45 PM
---
+++ exercicios-js/ex021Setters.js
@@ -0,0 +1,5 @@
+
+const dataAtual = new Date();
+dataAtual.setFullYear(1990);
+
+console.log(dataAtual)
