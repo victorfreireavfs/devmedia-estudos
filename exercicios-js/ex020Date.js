83518d0177036d84b814b999503cd607af39b2ca
Victor Freire <128263774+victorfreireavfs@users.noreply.github.com>
2024-06-18 22:11:11 -0300

    Função Date
    - manipulando o objeto Date

---
+++ exercicios-js/ex020Date.js
@@ -0,0 +1,28 @@
+const hoje = new Date();
+
+console.log(hoje.toTimeString()); 
+
+
+// const hoje = new Date();
+
+// console.log(hoje.valueOf()); ~
+
+// const dataAtual = new Date();
+// const dataPostagem = new Date('2020-11-11T00:00');
+
+// const timeAtual = dataAtual.getTime();
+// const timePostagem = dataPostagem.getTime();
+// const diferenca = timeAtual - timePostagem;
+
+// const segundos = diferenca / 1000;
+// const minutos = segundos / 60;
+// const horas = minutos / 60;
+
+// console.log(horas);
+
+
+// const dataAtual = new Date();
+
+// console.log( dataAtual.getTime() );
+// console.log( dataAtual.getTimezoneOffset() );
+// console.log( dataAtual.valueOf() );
