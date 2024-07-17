08a589c74134b1a26012d34020a6d5de8f868665
Victor Freire <128263774+victorfreireavfs@users.noreply.github.com>
2024-07-16 21:17:36 -0300

    revisando

--- exercicios-js/ex022Class.js
+++ exercicios-js/ex022Class.js
@@ -1,7 +1,56 @@
-class Humano{
-    fome = true
-}
+// class Humano{
+//     fome = true
+// }
 
-const pessoa = new Humano();
+// const pessoa = new Humano();
 
-console.log(pessoa.fome)
+// console.log(pessoa.fome)
+
+
+// ---------------------------------
+// FUNÇÕES
+
+// function cumprimentar(){
+//     console.log('bom dia', 2+3);
+//     // console.log(soma)
+//     // let soma = 2+3;
+// }
+
+// cumprimentar();
+
+// 
+
+// function anosPraMaiorIdade(idade){
+//     return 18 - idade;
+// }
+
+// const cauculoIdade = anosPraMaiorIdade(12);
+// console.log(cauculoIdade)
+
+// ------------------------------
+
+
+// FUNCTION.CALL E  APPLAY
+
+// function filhos(a, b){
+//     console.log(this)
+//     return a + b;
+// }
+
+// const pessoa = {
+//     nome: 'Victor'
+// }
+
+// console.log(filhos.call(pessoa, 1, 2));
+
+
+// ----------------------------
+
+// arrow functions
+
+// const quantosAnosFalta = (idade) => 18 - idade;
+// console.log(quantosAnosFalta(15))
+
+// -----------------------------
+
+
