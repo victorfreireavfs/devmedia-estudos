da058f44585521db642c797ea9523161d290f065
Victor Freire <128263774+victorfreireavfs@users.noreply.github.com>
2024-08-21 20:53:01 -0300

    Livro JS
    - adicionando eventos

--- exercicios-js-livro/cap06/ex002/scripts02.js
+++ exercicios-js-livro/cap06/ex002/scripts02.js
@@ -1,2 +1,15 @@
 // adicionar eventos ao DOM
+let button = document.getElementById('myButton');
+button.addEventListener('click', function(){
+    console.log('O botão foi clicado!');
+});
 
+let paragraph = document.getElementById('myParagraph');
+paragraph.addEventListener('mouseover', function(){
+    console.log('O mouse está sobre o paragrafo')
+});
+
+let input = document.getElementById('myInput');
+input.addEventListener('change', function(){
+    console.log('O valor do input foi alterado para' + this.value);
+});
