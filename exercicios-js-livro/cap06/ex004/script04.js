aa6b277792879d1b10279b0631dc6593fce015f8
Victor Freire <128263774+victorfreireavfs@users.noreply.github.com>
2024-11-11 21:59:08 -0300

    Estudos livro
    -retomando estudos e revisando DOM

--- exercicios-js-livro/cap06/ex004/script04.js
+++ exercicios-js-livro/cap06/ex004/script04.js
@@ -7,7 +7,7 @@ let h1 = document.getElementsByTagName("h1")[0];
 // selecionando pelo nome da tag
 
 let meuDiv2 = document.querySelector("#meuDiv");
-let minhaClasse2 = document.querySelector(".minhaClasse");
+let minhaClasse2 = document.querySelector("minhaClasse");
 let h1_2 = document.querySelector("h1");
 // usando query selector para selecionar os elementos
 
@@ -17,3 +17,17 @@ meuDiv.textContent = "Novo conteúdo para meu div";
 minhaClasse.innerHTML = "<strong>Novo conteúdo para minha classe </strong>";
 h1.textContent = "Novo conteúdo para meu H1.";
 
+// MANIPULANDO ATRIBUTOS
+document.getElementById("minhaImagem").setAttribute("src", "img/mascote.png");
+document.getElementById("meuLink").setAttribute("href", "https://www.youtube.com")
+
+// MANIPULANDO ESTILO
+meuDiv.style.backgroundColor="lightblue";
+h1.style.color="red";
+
+
+// TRABALHANDO COM EVENTOS
+document.getElementById("meuBotao").addEventListener("click", function(){
+
+    document.getElementById("meuParagrafo").style.color="green"
+});
