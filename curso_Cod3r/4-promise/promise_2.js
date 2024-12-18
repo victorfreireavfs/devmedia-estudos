bbfae1512dd3f9fb7f78e460a7a8955e17ad4e95
Victor Freire <128263774+victorfreireavfs@users.noreply.github.com>
2024-12-17 22:00:26 -0300

    Curso COD3R
    - desafio promise

---
+++ curso_Cod3r/4-promise/promise_2.js
@@ -0,0 +1,17 @@
+// function esperarPor(tempo = 2000){
+//     return new Promise (function(resolve){
+//         setTimeout(function(){
+//             console.log('executando promise...')
+//             resolve('vishhhh...')
+//         }, tempo)
+//     })
+// }
+
+// esperarPor()
+//     .then(() => esperarPor())
+//     .then(() => esperarPor())
+//     .then(() => esperarPor())
+
+
+// ---------------------
+
