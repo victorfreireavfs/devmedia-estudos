c0a63e806dcc7ae7e47815c367062799f18b3650
Victor Freire <128263774+victorfreireavfs@users.noreply.github.com>
2025-01-24 21:26:00 -0300

    App React
    - aprendendo fundamentos do React

---
+++ react-app/src/App.tsx
@@ -0,0 +1,14 @@
+// importo o componente e chammo como tag. 
+
+import './App.css'
+import Massage  from './massage'
+
+function App() {
+ return(
+  <div>
+    <Massage />
+  </div>
+ )
+}
+
+export default App
---
+++ react-app/src/massage.tsx
@@ -0,0 +1,11 @@
+// .tsx é a extensão do typescript do react
+//  crio a função usando o return e exporto o componente
+
+function Massage(){
+    const name = ''
+    if(name)
+    return <h1>Hello {name}</h1>
+    return <h1>Hello World</h1>
+}
+
+export default Massage
