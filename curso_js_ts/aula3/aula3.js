6ab886e36d59addc3cfca37c2ec0d9f9497498df
Victor Freire <128263774+victorfreireavfs@users.noreply.github.com>
2025-03-24 10:11:17 -0300

    Curso OtavioMiranda
    - revisão

---
+++ curso_js_ts/aula3/aula3.js
@@ -0,0 +1,1 @@
+console.log('Meu nome é "Victor". Estou revisando JavaScript às' , 10, 'da manhã.')
---
+++ curso_js_ts/aula4/comentarios.js
@@ -0,0 +1,9 @@
+// comentarios em js 
+console.log('olá mundo') // aqui tem outro comentário
+// console.log('olá mundo') // aqui tem outro comentário
+console.log('olá mundo') // aqui tem outro comentário
+console.log('olá mundo') 
+
+/*aqui tem outro comentário
+adaskdjaks  lo 
+*/
