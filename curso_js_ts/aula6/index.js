934e987f45a7600b95ab4dd1e8a8e0b0113325ff
Victor Freire <128263774+victorfreireavfs@users.noreply.github.com>
2025-03-25 20:49:29 -0300

    Curso TS JS
    - revisando regras de variáveis

---
+++ curso_js_ts/aula6/index.js
@@ -0,0 +1,27 @@
+let nome //sempre salve com let
+// tbm pode inicializar a variável para aciona-la posteriormente
+//sempre salve com let
+
+nome = 'Victor'
+
+console.log(`${nome} nasceu em 1984.`)
+console.log(`em 2000 ${nome} conheceu maria.`)
+console.log(`${nome} casou-se com maria em 2012`)
+console.log(`maria teve 1 filho com ${nome} em 2015`)
+console.log(`o filho de ${nome} chama-se eduardo`)
+
+nome = 'Pedro'
+console.log(`${nome} nasceu em 1984.`)
+console.log(`em 2000 ${nome} conheceu maria.`)
+console.log(`${nome} casou-se com maria em 2012`)
+console.log(`maria teve 1 filho com ${nome} em 2015`)
+console.log(`o filho de ${nome} chama-se eduardo`)
+
+/* regra pra criar variáveis: palavras reservadas
+    nomes significativos
+    não iniciar com number
+    letras minúsculas(exeto funções construtoras)
+    sem espaços ou traços
+    camelCase
+    Case-sensitive
+*/ 
