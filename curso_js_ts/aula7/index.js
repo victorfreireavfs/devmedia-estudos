14ba415f6c5803e87fed90a6cb1adc7b944d03ed
Victor Freire <128263774+victorfreireavfs@users.noreply.github.com>
2025-03-26 11:21:39 -0300

    Curso TS JS
    - revisando const

---
+++ curso_js_ts/aula7/index.js
@@ -0,0 +1,17 @@
+// CONST 
+
+const nome = 'victor'
+console.log(nome)
+
+const a = 5
+const b = 10
+const resultado = a * b
+console.log(resultado)
+
+resultadoDuplicado = resultado * 2
+console.log(resultadoDuplicado)
+
+resultadoTriplicado = resultado * 3
+console.log(resultadoTriplicado)
+
+console.log(typeof nome)
