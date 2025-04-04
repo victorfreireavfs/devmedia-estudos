dd169c623b9efee9c4a96fe377f1a19731afda5c
Victor Freire <128263774+victorfreireavfs@users.noreply.github.com>
2025-04-03 23:46:29 -0300

    Curso TS JS
    - resolvendo exercícios
    - formas de atribuir valores à variáveis

---
+++ curso_js_ts/aula12/aula12.js
@@ -0,0 +1,24 @@
+// let letraA = 'A'
+// let letraB = 'B'
+// let letraC = 'C'
+
+// console.log(letraA, letraB, letraC)
+
+// let letras = [letraA, letraB, letraC]
+// letraA = letras[1]
+// letraB = letras[2]
+// letraC = letras[0]
+
+// console.log(letraA, letraB, letraC)
+// // const letraATemp = letraA
+// // letraA = letraB
+// // letraB = letraC    
+// // letraC = letraATemp
+
+let varA = 'A'
+let varB = 'B'
+let varC = 'C';
+
+[varA, varB, varC] = [varB, varC, varA]
+
+console.log(varA, varB, varC)
