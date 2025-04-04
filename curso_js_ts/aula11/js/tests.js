96b8ebc9f273453b33f8ba78a2646bc82bda6582
Victor Freire <128263774+victorfreireavfs@users.noreply.github.com>
2025-04-03 22:30:47 -0300

    Curso TS JS
    - alert, confirm e prompt

---
+++ curso_js_ts/aula11/js/tests.js
@@ -0,0 +1,13 @@
+let num1 = prompt('digite o primeiro numero')
+let num2 = prompt('digite o segundo numero')
+
+num1 = Number(num1)
+num2 = Number(num2)
+
+let soma = num1 + num2
+let mult = num1 * num2
+let div = num1 / num2
+
+alert (`a soma dos dois numeros digitados é: ${soma}`)
+alert (`a multiplicação dos dois numeros digitados é: ${mult}`)
+alert (`a divisão dos dois numeros digitados é: ${div}`)
