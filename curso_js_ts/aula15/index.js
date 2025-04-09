73af8e7e5ab06e7dadcf5168f67a58156e3e8fe2
Victor Freire <128263774+victorfreireavfs@users.noreply.github.com>
2025-04-08 23:02:34 -0300

    Curso TS JS
    - revisando Math.

--- curso_js_ts/aula15/index.js
+++ curso_js_ts/aula15/index.js
@@ -1,4 +1,5 @@
-let num1 = 9.4643
+let num1 = 16
+
 // let num2 = Math.floor(num1) arredonda pra baixo
 // let num2 = Math.ceil(num1) arredonda pra cima
 // let num2 = Math.round(num1) arredonda para o n mais proximo da petade
@@ -11,6 +12,11 @@ let num1 = 9.4643
 // Math.random() gera um número entre/excluindo 0 e 1 
 
 // const ale = Math.round(Math.random() * (10 - 5) + 5);
-const ale = Math.floor(Math.random() * (10 - 5 + 1)) + 5;
+// const ale = Math.floor(Math.random() * (10 - 5 + 1)) + 5;
 
-console.log(ale);
+// console.log(Math.pow(2, 10)); POTENCIAÇÃO
+// console.log(2**10);
+
+// console.log(num1 ** 0.5) RAÍZ QUADRADA
+
+// console.log(100 / 0) DIVISÃO POR ZERO true infinity
