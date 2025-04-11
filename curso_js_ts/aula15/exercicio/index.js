ccdb7fc14df5391449b9ade2b19abd511f36b922
Victor Freire <128263774+victorfreireavfs@users.noreply.github.com>
2025-04-10 23:44:56 -0300

    Curso Backend JS TS
    - corrigindo exercício

--- curso_js_ts/aula15/exercicio/index.js
+++ curso_js_ts/aula15/exercicio/index.js
@@ -1,13 +1,15 @@
-let numero = prompt('Digite um número:');
-let numeroN = Number(numero)
+let numero = Number(prompt('Digite um número:'));
 
-    document.body.innerHTML += ` Seu número é: ${Number(numeroN)} <br/>`;
-    document.body.innerHTML += ` A raiz quarada dele é:  ${numeroN ** 0.5}.<br/>`;
-    document.body.innerHTML += ` Seu número ao quarado: ${numeroN ** 2 }. <br/>`;
-    document.body.innerHTML += ` Seu número é inteiro ? ${Number.isInteger(numeroN)} <br/>`;
-    document.body.innerHTML += ` Ele é NaN?  ${Number.isNaN(numeroN)} <br/>`;
-    document.body.innerHTML += ` Arredondando para cima:  ${Math.ceil(numeroN)}<br/>`;
-    document.body.innerHTML += ` Arredondando para baixo:  ${Math.floor(numeroN)} <br/>`;
-    document.body.innerHTML += ` Representação binária:  ${numeroN.toString(2)}<br/>`;
-    document.body.innerHTML += ` Duas casas decimais:  ${numeroN.toFixed(2)}<br/>`;
+let numeroTitulo = document.getElementById('numero-titulo')
+let texto = document.getElementById('texto')
+numeroTitulo.innerHTML = numero
+
+texto.innerHTML += ` A raiz quarada dele é:  ${numero ** 0.5}<br/>`;
+texto.innerHTML += ` Seu número ao quarado: ${numero ** 2 }<br/>`;
+texto.innerHTML += ` Seu número é inteiro ? ${Number.isInteger(numero)}<br/>`;
+texto.innerHTML += ` Ele é NaN?  ${Number.isNaN(numero)}<br/>`;
+texto.innerHTML += ` Arredondando para cima:  ${Math.ceil(numero)}<br/>`;
+texto.innerHTML += ` Arredondando para baixo:  ${Math.floor(numero)}<br/>`;
+texto.innerHTML += ` Representação binária:  ${numero.toString(2)}<br/>`;
+texto.innerHTML += ` Duas casas decimais:  ${numero.toFixed (2)}<br/>`;
 
